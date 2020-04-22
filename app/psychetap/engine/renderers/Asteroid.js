8ee4380cb6b33695bf024dc013fc44d665e70cf8
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-21 18:44:08 -0700

    feat: randomize asteroid images

--- app/psychetap/engine/renderers/Asteroid.js
+++ app/psychetap/engine/renderers/Asteroid.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import Matter from 'matter-js';
 
-import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../../utilities';
+import { SCREEN_WIDTH, SCREEN_HEIGHT, randomBetween } from '../../utilities';
 import { WORLD } from '../../engine/physicsInit';
+
+const asteroidImagePaths = [
+  require('../../../assets/psychetap/dangers/Asteroid1.png'),
+  require('../../../assets/psychetap/dangers/Asteroid2.png'),
+  require('../../../assets/psychetap/dangers/Asteroid3.png'),
+];
 
 const radius = Math.trunc(Math.max(SCREEN_WIDTH, SCREEN_HEIGHT) * 0.125) / 2;
 
@@ -11,12 +17,21 @@ const Create_Asteroid_Matter = (posX, posY) => {
   return Matter.Bodies.circle(posX, posY, radius, {
     collisionFilter: {
       category: 0x0002,
-      mask: 0x0001
-    }
+      mask: 0x0001,
+    },
   });
 };
 
 class Asteroid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageURI = this.asteroidImage;
+  }
+
+  get asteroidImage() {
+    return asteroidImagePaths[randomBetween(0, asteroidImagePaths.length - 1)];
+  }
+
   componentDidMount() {
     Matter.World.add(WORLD, [this.props.body]);
   }
@@ -40,11 +55,11 @@ class Asteroid extends React.Component {
             top: y,
             width: width,
             height: height,
-            borderRadius: width / 2
-          }
+            borderRadius: width / 2,
+          },
         ]}
       >
-        <Image source={require('../../../assets/images/projectiles/Asteroid.png')} style={styles.asteroid} />
+        <Image source={this.imageURI} style={styles.image} />
       </View>
     );
   }
@@ -56,9 +71,13 @@ const styles = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    width: radius,
-    height: radius
-  }
+    width: radius * 2,
+    height: radius * 2,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
 };
 
 export { Asteroid, Create_Asteroid_Matter };
