b767c0df7064766ca32044ad421b3612da607388
Lorenzo Faivre <lfaivre@asu.edu>
2020-04-21 20:43:14 -0700

    fix: sizing of psyche rover renderer

--- app/psychetap/engine/renderers/PsycheRover.js
+++ app/psychetap/engine/renderers/PsycheRover.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import { View, Image, StyleSheet} from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import Matter from 'matter-js';
-
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../../utilities';
 import { WORLD } from '../../engine/physicsInit';
 
-
+const psycheRoverImagePath = require('../../../assets/psychetap/spacecraft/Psyche.png');
 const PsycheRover_Matter = Matter.Bodies.rectangle(
   SCREEN_WIDTH / 2,
   SCREEN_HEIGHT * 0.75,
-  Math.trunc(SCREEN_WIDTH * 0.15),
-  Math.trunc(SCREEN_HEIGHT * 0.075),
+  Math.trunc(SCREEN_WIDTH * 0.3),
+  Math.trunc(SCREEN_HEIGHT * 0.15),
   {
     isStatic: true,
     collisionFilter: {
       category: 0x0001,
-      mask: 0x0002
+      mask: 0x0002,
     },
   }
 );
 
 class PsycheRover extends React.Component {
+  constructor(props) {
+    super(props);
+    this.psycheRoverImagePath = psycheRoverImagePath;
+  }
+
   componentDidMount() {
-    PsycheRover_Matter.render.sprite.texture = '../../assets/images/PsycheWhole.png';
+    PsycheRover_Matter.render.sprite.texture = this.psycheRoverImagePath;
     Matter.World.add(WORLD, [this.props.body]);
-    // console.log('PSYCHE NEW BODY: ', this.props.body.id);
-    // console.log('PSYCHE BOUNDS: ', this.props.body.bounds);
-    // console.log(this.props.body);
   }
 
   componentWillUnmount() {
@@ -34,8 +35,8 @@ class PsycheRover extends React.Component {
   }
 
   render() {
-    const width = Math.trunc(SCREEN_WIDTH * 0.15);
-    const height = Math.trunc(SCREEN_HEIGHT * 0.075);
+    const width = Math.trunc(SCREEN_WIDTH * 0.3);
+    const height = Math.trunc(SCREEN_HEIGHT * 0.15);
     const x = this.props.body.position.x - width / 2;
     const y = this.props.body.position.y - height / 2;
 
@@ -46,26 +47,30 @@ class PsycheRover extends React.Component {
           {
             left: x,
             top: y,
-            width: width,
-            height: height
-          }
-        ]}>
-           <Image source={require('../../../assets/images/PsycheWhole.png')} style={styles.psycheRover}/>
-        </View>
+            width,
+            height,
+          },
+        ]}
+      >
+        <Image source={this.psycheRoverImagePath} style={styles.image} />
+      </View>
     );
   }
 }
 
 const styles = {
   psycheRover: {
-    width: 150,
-    height: 200,
-    resizeMode: 'contain',
     position: 'absolute',
     bottom: 10,
+    top: Math.trunc(-SCREEN_HEIGHT / 3.7),
+    display: 'flex',
+    justifyContent: 'center',
     alignItems: 'center',
-    top: Math.trunc(-SCREEN_HEIGHT/3.7)
-  }
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
 };
 
 export { PsycheRover, PsycheRover_Matter };
